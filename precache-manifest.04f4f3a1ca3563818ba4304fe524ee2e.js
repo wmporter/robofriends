0d801f84de2647083fabe94b98c3d34a1184a38e
William Porter <bill@porterlabs.com>
2019-07-14 19:50:50 -0700

    Updates

---
+++ precache-manifest.04f4f3a1ca3563818ba4304fe524ee2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c28aa455c7615646a8bbad34e3ce1046",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "6bb2e0d8a898fc7ecbf4",
+    "url": "/robofriends/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/robofriends/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "6bb2e0d8a898fc7ecbf4",
+    "url": "/robofriends/static/js/main.6105fb89.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/robofriends/static/media/logo.5d5d9eef.svg"
+  }
+]);
